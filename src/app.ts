4743e604cf2b8ec46a6a632de090ab65da95aca6
Dang Chuong Pham <dangchuong1999@outlook.com>
2021-01-30 22:26:41 +0200

    Add router and /discovery route.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express from 'express'
 
+import discoveryRouter from './routers/discovery'
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello!')
+  res.send('Hello again!')
 })
+
+app.use('/discovery', discoveryRouter)
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}.`)
